fc98c7548616fe0b0b367d19d1188a1dbcdf73c7
Richard Ortiz <37150696+rortizs@users.noreply.github.com>
2023-03-05 15:36:53 -0600

    Primer Commit Agregando el Template

---
+++ website/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!-- Language -->
+<html lang="en">
+<!-- Head -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>PROYECTO ANÁLISIS DE SISTEMAS I - SANARATE</title>
+    /* Linking the css file to the html file. */
+    <!-- CSS Core theme Includes Boostrap -->
+    <link rel="stylesheet" href="./css/styles.css">
+    <!-- Font Awesome iconos (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+
+</body>
+</html>
